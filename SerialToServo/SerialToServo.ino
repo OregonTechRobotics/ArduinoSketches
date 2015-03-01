b5b8c67e43139994d73cd84f9ff4146f8fe31988
Christopher Dean <christopher.dean@oit.edu>
2015-03-01 00:05:24 -0800

    Improved Serial parse speed. Tested multiple servos.

--- SerialToServo/SerialToServo.ino
+++ SerialToServo/SerialToServo.ino
@@ -4,19 +4,13 @@ Servo  ServoX;
 Servo  ServoY;
 Servo  ServoZ;
 
-int BYTES_READ;
-int PARSE_INDEX;
-char DATA_BUFFER[601];
-int process_count;
+
 int READ_VALUE;
 char READ_ADDRESS;
-const int ADDRESS_WIDTH = sizeof(unsigned int);
 void setup() {
   // initialize serial:
-  Serial.begin(921600);
+  Serial.begin(115200);
 
-  BYTES_READ = 0;
-  PARSE_INDEX = 0;
   ServoX.attach(9);
   ServoY.attach(10);
   ServoZ.attach(11);
@@ -27,17 +21,14 @@ void setup() {
 
 void loop() {
   // if there's any serial available, read it:
-  if(Serial.available() > 0) 
+  if(Serial.available() >= 2) 
   {
-    BYTES_READ = Serial.readBytes(DATA_BUFFER,600);
     //Serial.print("Got ");
     //Serial.println(BYTES_READ);
-  }
-  process_count = 0;
-  while(process_count < (BYTES_READ/2))
-  {
-       READ_ADDRESS = DATA_BUFFER[PARSE_INDEX++];
-       READ_VALUE   = DATA_BUFFER[PARSE_INDEX++];
+    READ_ADDRESS = Serial.read();
+    READ_VALUE   = (int)Serial.read();
+    //Serial.print(READ_ADDRESS);
+    //Serial.println(READ_VALUE);
        switch(READ_ADDRESS)
        {
          case 'a':ServoX.write(READ_VALUE);break;
@@ -46,11 +37,9 @@ void loop() {
          case 13:break;
          default: Serial.println("Recieved incorrect address");break;
        }
-       process_count++;
+       delay(12);
   }
 
-  BYTES_READ = 0;
-  PARSE_INDEX = 0;
 }
 
 
